e037c198774648492b5fbf8515129a2831d6dcbf
Onsraa <truongteddy306@icloud.com>
2025-01-17 19:48:36 +0100

    add init ressources

--- src/plugins/simulation.rs
+++ src/plugins/simulation.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use crate::resources::bacterium::BacteriaData;
 use crate::resources::simulation::SimulationData;
+use crate::resources::window::*;
 use crate::states::simulation::SimulationState;
 use crate::systems::bacterium::*;
 
@@ -9,6 +10,8 @@ pub struct SimulationPlugin;
 impl Plugin for SimulationPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<SimulationState>();
+        app.init_resource::<CircleRadius>();
+        app.init_resource::<WindowSize>();
         app.init_resource::<BacteriaData>();
         app.init_resource::<SimulationData>();
         app.add_systems(Update, (destroy_bacteria, initialize_bacteria, spawn_bacteria).chain().run_if(in_state(SimulationState::Refreshing)));
